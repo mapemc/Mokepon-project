aa679965a5223a5f04e5864e2befa787dbc71563
nixhy <michaellipe@hotmail.com>
2023-10-10 19:40:39 -0500

    adicion

--- digitalHouse/microDesafios/funciones/ejercicio1.js
+++ digitalHouse/microDesafios/funciones/ejercicio1.js
@@ -1,28 +1,24 @@
-let tipoVehivulo = ["compacto", "mediano", "camioneta"]
-let diasAlquiler
-let sillaBebe = true
-
-
 function calculoVehiculo(tipoVehivulo, diasAlquiler, sillaBebe) {
     
     switch (tipoVehivulo) {
-    case value:
-        
-        break;
-   
-    default:
-        break;
-   } ( == "compacto") {
-        tipoVehivulo = 14000
-    } else if (tipoVehivulo == "mediano") {
-        tipoVehivulo = 17000
-    } else if (tipoVehivulo == "camioneta"){
-        tipoVehivulo = 28000
-    } else {
-        tipoVehivulo = 0};
-    sillaBebe === 1 ? 1200 : 0;
-    let resultado = tipoVehivulo + diasAlquiler * sillaBebe
-    return resultado
+        case "compacto":
+            tipoVehivulo = 14000
+            break;
+        case "mediano":
+            tipoVehivulo = 17000
+            break;
+        case "camioneta":
+            tipoVehivulo = 28000
+            break;
+    }
+
+   if (sillaBebe == 1) {
+    sillaBebe = 1200
+   } else {
+       sillaBebe = 0 
+   }
+    precioFinal = ((tipoVehivulo + sillaBebe) * diasAlquiler)
     
+    return console.log("Estimado cliente: en base al tipo de vehículo " + tipoVehivulo + " alquilado, considerando los " + diasAlquiler + " días utilizados, el monto total a pagar es de " + precioFinal);
 }
-console.log(calculoVehiculo(compacto,12,1))
+console.log(calculoVehiculo("camioneta", 28,1));
